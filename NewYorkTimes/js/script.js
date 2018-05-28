6aed151ebe00d93751ca403a39e1beca5dedb30c
abdulazizasiri <aasiri@lion.lmu.edu>
2018-05-28 07:14:07 -0700

    added a streetView for the udacity project

--- NewYorkTimes/js/script.js
+++ NewYorkTimes/js/script.js
@@ -6,11 +6,14 @@ function loadData() {
     var $nytHeaderElem = $('#nytimes-header');
     var $nytElem = $('#nytimes-articles');
     var $greeting = $('#greeting');
+    var $city = $("#city").val();
+    var $street = $("#street").val();
+    var $conCat = "http://maps.googleapis.com/maps/api/streetview?size=600x300&location="+$city+$street ;
 
     // clear out old data before new request
-    $wikiElem.text("");
-    $nytElem.text("");
-
+    $body.append('<img class="bgimg" src='+$conCat+">");
+    // $wikiElem.text("");
+    // $nytElem.text("");
     // load streetview
 
     // YOUR CODE GOES HERE!
