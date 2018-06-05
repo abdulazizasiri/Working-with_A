5955e4ed414208c871b33d00945bed6beedd030f
abdulazizasiri <aasiri@lion.lmu.edu>
2018-06-05 02:42:29 -0700

    added a reademe fro promise based api

--- ajaxRequests/async.js
+++ ajaxRequests/async.js
@@ -1,36 +1,7 @@
-// window.onload = function() {
-  let http = new XMLHttpRequest();
-  http.onreadystatechange = function() {
-    console.log(http);
-  }
-  http.open("GET", "data/tweets.json", true);
-  http.send();
+(function(){
 
+let http = new XMLHttpRequest();
+http.open("GET","data/tweets.json",false);
 
-};
-
-// STEPS
-
-/*
-1- request not initilize
-2- request has been set up
-3- request has been sent
-4- request is in process
-4- request is complete
-
-// SOURCE:  the youtuber, the net ninja
-*/
-
-
-
-// callback function
-
-
-  window.onload = function() {
-
-    $.getJSON("data/tweets.json",function(data){
-      console.log(datalist);
-    });
-    console.log("TEST")
-
- }
+http.send();
+}())
