197b0cda47bd5d9320b8c57cab1e02043ed460c4
abdulazizasiri <aasiri@lion.lmu.edu>
2018-06-07 02:19:28 -0700

    factoring out some code in seperate files

---
+++ GoogleMaps/js/app.js
@@ -0,0 +1,8 @@
+// Add a IIFE later for seucring the app.
+  var map;
+  function initMap() {
+    map = new google.maps.Map(document.getElementById('map'),{
+      center: {lat: 33.955654, lng:-118.416721},
+      zoom: 10
+    });
+  }
