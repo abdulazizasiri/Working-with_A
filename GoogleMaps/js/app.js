2dc412ef78d5ff5a00e7de944d16c0d3d3908484
abdulazizasiri <aasiri@lion.lmu.edu>
2018-10-08 02:54:40 -0700

    things i did not push

---
+++ GoogleMaps/js/app.js
@@ -0,0 +1,96 @@
+// Add a IIFE later for seucring the app.
+  var map;
+  function initMap() {
+    var styles = [
+      {
+        featureType: 'water',
+        stylers: [
+          { color: '#0000d8' }
+        ]
+      },{
+        featureType: 'administrative',
+        elementType: 'labels.text.stroke',
+        stylers: [
+          { color: '#ffffff' },
+          { weight: 6 }
+        ]
+      },{
+        featureType: 'administrative',
+        elementType: 'labels.text.fill',
+        stylers: [
+          { color: '#e85113' }
+        ]
+      },{
+        featureType: 'road.highway',
+        elementType: 'geometry.stroke',
+        stylers: [
+          { color: '#efe9e4' },
+          { lightness: -40 }
+        ]
+      },{
+        featureType: 'transit.station',
+        stylers: [
+          { weight: 9 },
+          { hue: '#e85113' }
+        ]
+      },{
+        featureType: 'road.highway',
+        elementType: 'labels.icon',
+        stylers: [
+          { visibility: 'off' }
+        ]
+      },{
+        featureType: 'water',
+        elementType: 'labels.text.stroke',
+        stylers: [
+          { lightness: 100 }
+        ]
+      },{
+        featureType: 'water',
+        elementType: 'labels.text.fill',
+        stylers: [
+          { lightness: -100 }
+        ]
+      },{
+        featureType: 'poi',
+        elementType: 'geometry',
+        stylers: [
+          { visibility: 'off' },
+          { color: '#f0e4d3' }
+        ]
+      },{
+        featureType: 'road.highway',
+        elementType: 'geometry.fill',
+        stylers: [
+          { color: '#efe94' },
+          { lightness: -25 }
+        ]
+      }
+    ];
+
+    map = new google.maps.Map(document.getElementById('map'),{
+      center: {lat: 33.955654, lng:-118.416721},
+      zoom: 10,
+      styles: styles,
+      mapTypeControl: true
+
+    });
+
+    var location = {lat: 33.955654, lng:-118.416721};
+    var marker = new google.maps.Marker({
+      position: location,
+      map:map,
+      title: 'My location',
+      animation: google.maps.Animation.DROP
+    });
+
+    var info = new google.maps.InfoWindow({
+      content:location.lat + location.lng
+    });
+
+    // this is an event listerm
+
+    marker.addListener("click", function(){
+      info.open(map, marker);
+    })
+  }
