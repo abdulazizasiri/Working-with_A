bd965d640b134393b6ea11cb6812653b7567e4fd
abdulazizasiri <aasiri@lion.lmu.edu>
2018-10-08 03:16:40 -0700

    remove unnecessary files

--- NewYorkTimes/js/script.js
+++
@@ -1,8 +0,0 @@
-window.onload = function() {
-
-  $.getJSON("data/tweets.json",function(data){
-    console.log(datalist);
-  });
-  console.log("TEST")
-
-}
--- ajaxRequests/async.js
+++
@@ -1,7 +0,0 @@
-(function(){
-
-let http = new XMLHttpRequest();
-http.open("GET","https://api.unsplash.com/search/photos?page=1&query=flowers",false);
-// fetch('https://api.unsplash.com/search/photos?page=1&query=flowers');
-http.send();
-}())
